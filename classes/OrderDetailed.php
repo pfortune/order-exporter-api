ca080bfbf4327217e24b7ca881fad2f557fdad98
Peter Fortune <pfortune@gmail.com>
2019-10-16 01:59:07 +0100

    Added getOrderMessage method; message and id_carrier added to response

--- classes/OrderDetailed.php
+++ classes/OrderDetailed.php
@@ -14,21 +14,41 @@ class OrderDetailed extends ObjectModel
         
     private function getOrder()
     {
-        $orderId = $_GET['orderId'];
+        $orderId = $_GET['orderid'];
         $objOrder = new Order($orderId);
         $orderDetailResponse = '';
         if (empty($objOrder->id)) {
-            Json::generate(400, "error", "You have passed wrong orderId.");
+            Json::generate(400, "error", "You have passed wrong order id.");
         } else {
             $orderDetailResponse = $this->getOrderDetails($orderId);
         }
         
         Json::generate(200, "success", "Order details fetched successfully.", $orderDetailResponse);
     }
+
+    private function getOrderMessage($orderId)
+    {
+        $sql = "SELECT * FROM `"._DB_PREFIX_."message` WHERE id_order = $orderId AND private=0";
+        $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
+
+        
+        return $result;
+    }
     
     private function getOrderDetails($orderId)
     {
         $orderDetails = new Order($orderId);
+        
+        $orderMessages = array();
+        $messages = $this->getOrderMessage($orderId);
+
+        if (!empty($messages)) {
+            foreach ($messages as $message) {
+                $orderMessages[] = array(
+                    'message' => $message['message']
+                );
+            }
+        }
         
         $itemlist = array();
         $OrderProductsList = $orderDetails->getOrderDetailList();
@@ -56,21 +76,23 @@ class OrderDetailed extends ObjectModel
         $countryIso = CountryCore::getIsoById($addressInfo->id_country);
         
         $resultOrderResponse = array(
-            'company'		=> $addressInfo->company,
-            'firstname'		=> $CustomerCoreDetail->firstname,
-            'lastname'		=> $CustomerCoreDetail->lastname,
-            "id" 			=> (int) $orderId,
-            "date_add"		=> $orderDetails->date_add,
-            'id_customer'   => $orderDetails->id_customer,
-            'address1'		=> $addressInfo->address1,
-            'address2'		=> $addressInfo->address2,
-            'city'			=> $addressInfo->city,
-            'state'			=> $state,
-            'postcode'		=> $addressInfo->postcode,
-            'countryIso'	=> $countryIso,
-            'email'			=> $CustomerCoreDetail->email,
-            'phone' 		=> $phone,
-            "itemslist" 	=> $itemlist
+            "id_carrier"=> $orderDetails->id_carrier,
+            'company'   => $addressInfo->company,
+            'firstname' => $CustomerCoreDetail->firstname,
+            'lastname'  => $CustomerCoreDetail->lastname,
+            "id" 		=> (int) $orderId,
+            "date_add"	=> $orderDetails->date_add,
+            'id_customer' => $orderDetails->id_customer,
+            'address1'	=> $addressInfo->address1,
+            'address2'	=> $addressInfo->address2,
+            'city'		=> $addressInfo->city,
+            'state'		=> $state,
+            'postcode'	=> $addressInfo->postcode,
+            'countryIso'=> $countryIso,
+            'email'		=> $CustomerCoreDetail->email,
+            'phone' 	=> $phone,
+            'itemslist' => $itemlist,
+            'message' => $orderMessages[0]['message']
         );
 
         return $resultOrderResponse;
